91389ca4d26bc949718db36991f128b0384c8ea4
jingshuang1 <jingshuang1@staff.weibo.com>
2023-09-22 12:07:21 +0800

    node-xlsx can be work to create a buffer data  then  fs to create xlsx file

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,6 @@
+var xlsx = require('node-xlsx')
 var router = require('koa-router')();
+const fs = require('fs');
 
 router.get('/', function *(next) {
   yield this.render('index', {
@@ -14,9 +16,20 @@ router.get('/foo', function *(next) {
 
 router.get('/one', function *(next){
   this.type = 'application/json';
-  yield this.body ={
-    title:'render',
-    msg: 20
+  const data = [
+    [],
+    [1, 2, 3],
+    [true, false, null, 'sheetjs'],
+    ['foo', 'bar', new Date('2014-02-19T14:30Z'), '0.3'],
+    ['baz', null, 'qux'],
+  ];
+  const sheetOptions = {'!cols': [{wch: 6}, {wch: 7}, {wch: 10}, {wch: 20}]};
+  var buffer = xlsx.build([{name: 'mySheetName', data: data}], {sheetOptions}); // Returns a buffer
+  const filePath = './file.xlsx';
+  fs.writeFileSync(filePath, buffer, 'binary');
+  yield this.body = {
+    render: 'render',
+    msg:200
   }
 });
 
